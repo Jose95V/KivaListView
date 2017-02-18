63505935f7f741a183313f8b27b9c30c73c46445
Jose Vallecillo <jose.vallecillo@ujcv.edu.hn>
2017-02-17 21:10:11 -0600

    creacion de clase MySingleton

--- app/src/main/java/com/example/josedanilo/kivalistview/MainActivity.java
+++ app/src/main/java/com/example/josedanilo/kivalistview/MainActivity.java
@@ -1,9 +1,25 @@
 package com.example.josedanilo.kivalistview;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
